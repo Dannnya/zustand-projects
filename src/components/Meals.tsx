d6f0ad2cc95c3e17d453d20d7d2efb1b1ac6e5a6
Dannnya <komarovskiy.daniil23@gmail.com>
2025-07-02 23:56:32 +0300

    update: Meals Component

--- src/components/Meals.tsx
+++ src/components/Meals.tsx
@@ -4,9 +4,41 @@ import { useStoreMeals } from '../store';
 export const Meals = () => {
 
     const { meals, searchQuery, setMeals, setSearchQuery } = useStoreMeals();
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const response = await fetch('https:/www.themealdb.com/api/json/v1/1/filter.php?c=Seafood');
+                const data = await response.json();
+                setMeals(data.meals)
+            } catch (error) {
+                console.error('Error:', error);
+            };
+        };
+
+        fetchMeals();
+    }, [setMeals]);
+
+    const filteredMeals = meals.filter(meal => meal.strMeal.toLowerCase().includes(searchQuery.toLowerCase()));
     return (
         <div>
-      
+            <h4>Seafood Recipes</h4>
+            
+            <input type='text' placeholder='Search for a meal...' value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+            />
+
+            <div>
+                {filteredMeals.length >= 0 ? (
+                    filteredMeals.map(meal => (
+                        <div key={meal.idMeal}>
+                            <img src={meal.strMealThumb} alt={meal.strMeal} width={300}  className='pt-5'/>
+                        </div>
+                    ))
+                ): (
+                    <p>No meals found</p>
+                ) }
+            </div>
         </div>
     )
 };
